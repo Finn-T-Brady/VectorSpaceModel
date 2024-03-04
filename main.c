d6bba7d7550f18b0296bd2d4543d55842c2a8574
Finn Brady <finn.t.brady@gmail.com>
2024-03-04 15:51:54 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-#ifndef VecChunk_MAX
-#define VecChunk_MAX 8
-#endif
-
-typedef struct List_0{
-	void* X;
-	struct List_0* next;
-} List;
-
-List** NewNCyc(int n){
-	List* Head = calloc(n,sizeof(List));
-	int x = n-1;
-	while(x--){
-		Head[n].next=&Head[n+1];
-	}
-	Head[n-1].next=Head;
-	return &Head;
-}
-typedef struct VecChunk_1 VecKey;
-typedef struct VecChunk_0{
-	int n;
-	VecKey* items;
-	struct VecChunk_0* next;
-} VecChunk;
-
-struct VecChunk_1{
-	char* key;
-	int n;
-};
-
-VecChunk StrDecomp(char* in){
-	//
-	//
-}
+#include "VectorSpace.h"
 
 int main(){
-	char *a = "This is a test string, its goal is to test this program";
+	char *TestStr = "This is a test string, its goal is to test this program. AAAA... \n I hear it's amazing when the famous stuffed purple worm in flap-jaw space, with the tuning fork... does a raw blink off Hara-Kiri rock. I need scissors! 61!";
 
-	VecChunk J;
-	J.items=calloc(sizeof(VecKey),8);
-	J.items[4].key="aaa";
-	J.items[4].n=1;
+	char *TestStr2 = "The Purpose of this String is to Test part of this Program,61... Raiden, turn off the game console.";
+	       
+	VecChunk *Vec_TestStr = StrDecomp(TestStr,2,32);
+	VecChunk *Vec_TestStr2 = StrDecomp(TestStr2,1,32);
 
+	printf("Test Vector A:\n");
+	VecDump(Vec_TestStr);
+	printf("\nTest Vector B:\n");
+	VecDump(Vec_TestStr2);
+	printf("\n");
+	
+	int dp = VecDotProduct_unsorted(Vec_TestStr,Vec_TestStr2);
+	
+	double norm_A,norm_B;
+	norm_A=VecNorm(Vec_TestStr);
+	norm_B=VecNorm(Vec_TestStr2);
+
+	printf("A,B dot product:%i\n",dp);
+	printf("A norm:%lf\nB norm:%lf\n",norm_A,norm_B);
+
+	double cs = VecCosine_unsorted(Vec_TestStr,Vec_TestStr2);
+	double scs = VecCosine_unsorted(Vec_TestStr,Vec_TestStr);
+
+	printf("A-B cosine:%lf\nA-A cosine(should be 1):%lf\n",cs,scs);
+
+	
 	return 0;
 }
