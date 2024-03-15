6060f27d1eb5bb89c2c2a69ec97a25c22de8c171
Finn Brady <finn.t.brady@gmail.com>
2024-03-15 05:47:36 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,21 +6,26 @@
 #include "VectorSpace.h"
 
 int main(){
-	char *TestStr = "This is a test string, its goal is to test this program. AAAA... \n I hear it's amazing when the famous stuffed purple worm in flap-jaw space, with the tuning fork... does a raw blink off Hara-Kiri rock. I need scissors! 61!";
+	char *TestStr = "Hello, my name is Joel, I work in Computer Science, I program mainly in Haskell, I specialise in researching hardware development. I Have published several papers regarding types of hardware, weather they are commonly used or not.";
 
-	char *TestStr2 = "The Purpose of this String is to Test part of this Program,61... Raiden, turn off the game console.";
-	       
-	VecChunk *Vec_TestStr = StrDecomp(TestStr,2,32);
-	VecChunk *Vec_TestStr2 = StrDecomp(TestStr2,1,32);
+	char *TestStr2 = "Greetings, I am Joey, I am working in Biology, I am branching out in to wetware research, wetware is when biological life is used in computer hardware. I believe that fungal wetware is an amazing development in computer hardware";
+	
+
+	VecChunk *Vec_TestStr = StrDecomp(TestStr,32);
+	VecChunk *Vec_TestStr2 = StrDecomp(TestStr2,32);
+
+	VecMergeSort(Vec_TestStr);
+	VecMergeSort(Vec_TestStr2);
+	
 
 	printf("Test Vector A:\n");
 	VecDump(Vec_TestStr);
 	printf("\nTest Vector B:\n");
 	VecDump(Vec_TestStr2);
 	printf("\n");
-	
-	int dp = VecDotProduct_unsorted(Vec_TestStr,Vec_TestStr2);
-	
+		
+	int dp = VecDotProduct(Vec_TestStr,Vec_TestStr2);
+
 	double norm_A,norm_B;
 	norm_A=VecNorm(Vec_TestStr);
 	norm_B=VecNorm(Vec_TestStr2);
@@ -28,11 +33,10 @@ int main(){
 	printf("A,B dot product:%i\n",dp);
 	printf("A norm:%lf\nB norm:%lf\n",norm_A,norm_B);
 
-	double cs = VecCosine_unsorted(Vec_TestStr,Vec_TestStr2);
-	double scs = VecCosine_unsorted(Vec_TestStr,Vec_TestStr);
+	double cs = VecCosine(Vec_TestStr,Vec_TestStr2);
+	double scs = VecCosine(Vec_TestStr,Vec_TestStr);
 
 	printf("A-B cosine:%lf\nA-A cosine(should be 1):%lf\n",cs,scs);
-
 	
 	return 0;
 }
